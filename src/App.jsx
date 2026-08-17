e44550728eada09b5baa6e2c2ef1f4200354f499
bhoiswati1106-sys <bhoiswati1106@gmail.com>
2026-08-17 11:23:06 +0530

    Fix Projects import path

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
-import Projects from "./components/Projects";
+import Projects from "./Components/Projects";
 import Experience from "./components/Experience";
 import Certificates from "./components/Certificates";
 import Education from "./components/Education";
